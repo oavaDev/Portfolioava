ae2418427418b44fdf8c8cc5f34fb07af82bbe67
Omar Vega <omarvega598@gmail.com>
2023-02-02 10:03:57 -0500

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
+'use client';
 import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import Navbar from '../components/Navbar';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <head />
-      <body>{children}</body>
+    <html lang='en'>
+      <head>
+        <title>Portfolio</title>
+      </head>
+      <body>
+        <ThemeProvider enableSystem={true} attribute='class'>
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
