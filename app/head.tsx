5ffcbefdc8c4463f57d3d324afa31619fb3e3f5c
cCeth <omarvega598@gmail.com>
2023-03-09 23:46:48 -0500

    add metatag description

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,12 @@
 export default function Head() {
   return (
     <>
-      <title>Omar Vega Portfolio</title>
+      <meta name='title' content='Omar Vega Portfolio' />
       <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta
+        name='description'
+        content="Welcome, I'd like to introduce myself through this portfolio and show you some of the projects I've done"
+      />
       <link
         rel='icon'
         href='https://avatars.githubusercontent.com/u/83027913?v=4'
